f21641460443aad203f8e55c12eaeb2bcc345e8e
Steve Merrony <merrony@gmail.com>
2019-05-05 15:42:15 +0200

    Update silly test

--- loadg/loadg_test.go
+++ loadg/loadg_test.go
@@ -3,8 +3,7 @@ package main
 import "testing"
 
 func TestGetKnownEntryTypes(t *testing.T) {
-	kfet := KnownFstatEntryTypes()
-	fmtf := kfet[2]
+	fmtf := KnownFstatEntryTypes[2]
 	if "FMTF" != fmtf.DgMnemonic {
 		t.Errorf("Expected 'FMTF', got '%s'", fmtf.DgMnemonic)
 	}
